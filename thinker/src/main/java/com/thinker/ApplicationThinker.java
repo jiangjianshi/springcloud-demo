2f1b2f8401fcedab8748a94dcdb92ff3c8d01746
jiangjianshi <jianshi.jiang@100credit.com>
2019-10-23 10:45:15 +0800

    post

--- thinker/src/main/java/com/thinker/ApplicationThinker.java
+++ thinker/src/main/java/com/thinker/ApplicationThinker.java
@@ -1,10 +1,13 @@
 package com.thinker;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -15,15 +18,25 @@ import org.springframework.context.annotation.ComponentScan;
  * @Version V1.0
  */
 
-@RibbonClient(name="show-demo")
+//@RibbonClient(name="show-demo")
+//@EnableEurekaClient
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients(basePackages= {"com.show"})
+@EnableFeignClients(basePackages = {"com.show"})
 @ComponentScan("com")
 public class ApplicationThinker {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationThinker.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean ServletRegistrationBeangetServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
